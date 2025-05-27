c3c37012ee21b77191b26f68c0d164133e0f2b19
Ulrike Lehmann <lehmann13@uni-potsdam.de>
2025-05-27 15:24:12 +0200

    config.py

--- spiro/config.py
+++ spiro/config.py
@@ -22,9 +22,9 @@ class Config(object):
         'focus': 250,	     # default focus distance
         'password': '',      # an empty password will trigger password initialization for web ui
         'secret': '',        # secret key for flask sessions
-        'dayshutter': 100,   # day exposure time in fractions of a second, e.g. 100 means 1/100
+        'dayshutter':  6000000, # 6 seconds in microseconds
         'dayiso': 50,        # daytime iso values
-        'nightshutter': 10,  # night exposure time
+        'nightshutter': 6000000,  # 6 seconds in microseconds
         'nightiso': 400,     # night iso
         'name': 'spiro',     # the name of this spiro instance
         'debug': False,      # debug logging
